c87eb7408157ab63f0fb3c5865215ebc089c999b
crr0004 <crr0004@gmail.com>
2016-06-14 17:31:54 +1000

    Created documentation for polygon class

--- src/polygon.cpp
+++ src/polygon.cpp
@@ -7,14 +7,51 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
+/* -------------------------------*/
+/** 
+ * @brief A wrapper class for basic opengl buffered index drawing
+ * This is to ease the creation of a basic setup for something that draws just triangles.
+ *
+ * @b Example usage
+ * @code
+	Polygon triangle; 
+##Setup
+	triangle.setVertices(triangle_one_vertices, sizeof(triangle_one_vertices) / sizeof(GLfloat));
+	triangle.setIndices(triangle_one_indices, sizeof(triangle_one_indices) / sizeof(GLuint));
+	triangle.buildStatic();
+
+	triangle.setShaderLocations(vertShaderLocation);
+
+##Drawing
+	triangle.update(&logicContext);
+	triangle.draw(&logicContext);
+	@endcode
+ */
+/* ---------------------------------*/
 Polygon::Polygon(){
 }
 
+
+/* -------------------------------*/
+/** 
+ * @brief Sets the vertices to be used when rendering. Expects 3 elements per vertex
+ * @warning Only stores a reference to the vertices. It is up to you to keep it in allocated.
+ * @note Might change in the future.
+ * 
+ * @param vertices[] pointer to the vertices
+ * @param size how many elements are in the array
+ */
+/* ---------------------------------*/
 void Polygon::setVertices(GLfloat vertices[], unsigned int size){
 	vertexSize = size;
 	this->vertices = vertices;
 }
-
+/* -------------------------------*/
+/** 
+ * @see Polygon::setVertices(GLfloat, unsigned int)
+ */
+/* ---------------------------------*/
 void Polygon::setIndices(GLuint indices[], unsigned int size){
 	indicesSize = size;
 	this->indices = indices;
@@ -33,6 +70,11 @@ void Polygon::translate(glm::vec3 moveBy){
 	model_matrix = glm::translate(model_matrix, moveBy);
 }
 
+/* -------------------------------*/
+/** 
+ * @brief Builds the parts needed to draw static indexed triangles
+ */
+/* ---------------------------------*/
 void Polygon::buildStatic(){
 	glGenBuffers(2, &vboID[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
@@ -51,6 +93,15 @@ void Polygon::setShaderLocations(GLuint vertShaderLocation){
 
 }
 
+/* -------------------------------*/
+/** 
+ * @brief Performs the logic for the polygon
+ * @note Only stores and updates the model matrix at the moment
+ * @warning Only call a polygon's update one at a time after drawing because this pushs the current modelview onto the matrix stack
+ * 
+ * @param state current logical context containing references to the camera modelview matrix
+ */
+/* ---------------------------------*/
 void Polygon::update(struct LogicContext* state){
 	MatrixStackSingleton* instance = MatrixStackSingleton::instance();
 
@@ -59,6 +110,16 @@ void Polygon::update(struct LogicContext* state){
 
 }
 
+/* -------------------------------*/
+/** 
+ * @brief Draws the built triangles
+ * Binds the passed in arrays to opengl buffers. Binds the needed components for the shader, and draws the polygon
+ *
+ * @warning Must be directly called after update because it restores the modelview.
+ * 
+ * @param state @see Polygon::draw()
+ */
+/* ---------------------------------*/
 void Polygon::draw(struct LogicContext* state){
 		glUniformMatrix4fv(state->uloc_modelview, 1, GL_FALSE, glm::value_ptr(model_matrix));
 		glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
