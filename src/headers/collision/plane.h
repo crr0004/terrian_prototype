3530e5510de8e3a781f457045a0a8c7a5f004ec6
crr0004 <crr0004@gmail.com>
2016-07-14 17:35:56 +1000

    Added function to handle plane and line segment collision detection

---
+++ src/headers/collision/plane.h
@@ -0,0 +1,23 @@
+#ifndef COLLISION_PLANE
+#define COLLISION_PLANE 1
+#include <glm/glm.hpp>
+namespace Collision{
+	struct Plane{
+		float d;
+		glm::vec3 n;
+	};
+	static int colides(const glm::vec3 a, const glm::vec3 b, const Plane p, float &t, glm::vec3 &q){
+		glm::vec3 ab = b - a;
+		t = (p.d - glm::dot(p.n, a)) / glm::dot(p.n, ab);
+
+		if(t >= 0.0f && t <= 1.0f){
+			q = a + t * ab;
+			return 1;
+		}
+
+		return 0;
+
+	}
+
+};
+#endif /* ifndef COLLISION_POLYGON */
---
+++ src/headers/collision/point.h
@@ -0,0 +1,11 @@
+#ifndef COLLISION_POINT
+#define COLLISION_POINT 1
+namespace Collision{
+
+	class Point{
+		
+
+	};
+
+};
+#endif /* ifndef COLLISION_POINT */
