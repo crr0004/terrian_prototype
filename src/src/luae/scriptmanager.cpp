0a537a8e56a28bd1fff86ecb7c457cd96d5178f6
crr0004 <crr0004@gmail.com>
2017-03-17 23:12:57 +1100

    Added sample comment code for setting upvalues

--- src/src/luae/scriptmanager.cpp
+++ src/src/luae/scriptmanager.cpp
@@ -1,5 +1,6 @@
 #include "luae/scriptmanager.hpp"
 #include <lua/lua.hpp>
+#include <fmt/format.h>
 
 using namespace Luae;
 ScriptManager* ScriptManager::_instance = 0;
@@ -19,11 +20,28 @@ lua_State* ScriptManager::getState() {
 	return state;
 }
 void ScriptManager::NewMetaLib(const struct luaL_Reg lib[], const char* name){
+	/*
+	int nup = 0;
+	fmt::printf("Table count %d\n", sizeof(lib)/sizeof((lib)[0]) -1);
+	lua_createtable(state, 0, sizeof(lib)/sizeof((lib)[0]) -1);
+	luaL_checkversion(state);
+	luaL_checkstack(state, nup, "too many upvalues");
+	for (; lib->name != NULL; lib++) {  // fill the table with given functions
+		int i;
+		for (i = 0; i < nup; i++)  // copy upvalues to the top
+			lua_pushvalue(state, -nup);
+		lua_pushcclosure(state, lib->func, nup);  // closure with those upvalues
+		lua_setfield(state, -(nup + 2), lib->name);
+	}
+	lua_pop(state, nup);  // remove upvalues
+	*/
 	luaL_newmetatable(state, name);
-	luaL_setfuncs(state, lib, 0);
+	lua_pushstring(state, "hello");
+	luaL_setfuncs(state, lib, 1);
 	lua_setfield(state, -1, "__index");
 }
 void ScriptManager::NewLib(const struct luaL_Reg lib[], const char* name) {
+	luaL_newlibtable(state,lib);
 	luaL_newlib(state, lib);
 	lua_setglobal(state, name);
 }
