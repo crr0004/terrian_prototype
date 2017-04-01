4eabfcf646b81e86accf28879db2c3da889c1060
crr0004 <crr0004@gmail.com>
2017-04-01 21:48:46 +1100

    Added tests for new script functionality
    Added test for loading script from string
    Added test saving and restoring script state
    Added support for closing lua state

--- tests/luaescripttest.cpp
+++ tests/luaescripttest.cpp
@@ -1,15 +1,20 @@
 #include <catch.hpp>
 #include <string>
 #include <cstring>
+#include <lua/lua.hpp>
+#include "luae/scriptmanager.hpp"
 #include "luae/script.hpp"
+#include "luae/Utilities.hpp"
 
 TEST_CASE("Load script"){
+	Luae::ScriptManager::instance();
+
 	SECTION("Load named script"){
 		
 		Luae::Script* script = Luae::Script::Load("update.lua");
 
 		REQUIRE(script != NULL);
-INFO("Script name is " << script->getScriptName());
+		INFO("Script name is " << script->getScriptName());
 		int result = std::strcmp(script->getScriptName(), "update.lua");
 
 		REQUIRE(result == 0);
@@ -20,8 +25,16 @@ INFO("Script name is " << script->getScriptName());
 	SECTION("Failed load script"){
 		Luae::Script* script = Luae::Script::Load(std::string("doesnt_exist.lua"));
 		REQUIRE(script == NULL);
-		delete script;
 	}
+	SECTION("Load script from string"){
+		const char* source =
+			"function update()\n"
+			"--print(\"update called\")\n"
+			"end";		
+		Luae::Script* script = Luae::Script::Load("test", source);
+		REQUIRE(script != NULL);
+	}
+	Luae::ScriptManager::Close();
 
 }
 
@@ -47,10 +60,45 @@ TEST_CASE("Call functions"){
 
 		REQUIRE(result);
 
+		delete script;
 	}
 	SECTION("Fail called name function"){
 		Luae::Script* script = Luae::Script::Load("update.lua");
 		bool result = script->call("uupdate");
 		REQUIRE(!result);
+		delete script;
 	}
+	Luae::ScriptManager::Close();
+}
+
+
+TEST_CASE("Pop and push lua state"){
+	const char* source =
+		"hello = 2\n"
+		"--header = {\n"
+		"--functions = {update=update, update2=update2},\n"
+		"--variables = {test}\n"
+		"--}\n"
+		"";
+	const char* source2 =
+		"hello = 1\n"
+		"";
+
+	lua_State* lua = Luae::ScriptManager::instance()->getState();
+	Luae::Script* script = Luae::Script::Load("Pop lua state", source);
+	REQUIRE(script != NULL);
+	Luae::Script* script2 = Luae::Script::Load("Second script", source2);
+	//Should restore hello as 2
+	script->push();
+	lua_getglobal(lua, "hello");
+	int hello = (int)lua_tonumber(lua, -1);
+	REQUIRE(hello == 2);
+	script->pop();
+	script2->push();
+
+	lua_getglobal(lua, "hello");
+	hello = (int)lua_tonumber(lua, -1);
+	REQUIRE(hello == 1);
+	lua_pop(lua,2);
+	Luae::ScriptManager::Close();
 }
